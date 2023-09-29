c0e3a08f969d4bd25bb9b1deb02bf63fd1cbcdac
mp022 <mpferreiradesouza@gmail.com>
2023-09-29 16:00:34 -0300

    Foi adicionado um codigo de teste

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
 print(99)
+
+from pyspark.sql import SparkSession
+spark = SparkSession.builder \
+    .appName("HelloLines") \
+    .getOrCreate()
+sc = spark.sparkContext
+rdd = sc.textFile("hdfs://localhost:9000/datasets/spotify/tracks.json")
+df = spark.read.json(rdd)
+print(df["duration_ms"][1])
+sc.stop()
+
+print(99)
