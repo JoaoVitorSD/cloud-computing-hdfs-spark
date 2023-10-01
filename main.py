755c4e3deccccfe1eb6fa6b8d74f4ef7a3b98b84
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2023-10-01 17:13:00 -0300

    feat: removing outliers from analisis

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, exp
 spark = SparkSession.builder.appName("HelloLines").getOrCreate()
+spark.sparkContext.setLogLevel("WARN")
 sc = spark.sparkContext
-rdd = sc.textFile("hdfs://localhost:9000/datasets/spotify/")
-lines = rdd.count()
-outrdd = sc.parallelize([lines])
-# The following will fail if the output directory exists:
+# STEP - 1 - Songs Duration 
+# Removing outliers
+tracks = spark.read.json("hdfs://localhost:9000/datasets/spotify/tracks.json")
+print("size "+ str(tracks.count()))
+quantiles = tracks.approxQuantile("duration_ms", [0.25, 0.75],0.0);
+q1, q3 = quantiles[0], quantiles[1]
+iqr = q3 - q1
+lower_bound = q1 - 1.5 * iqr
+upper_bound = q3 + 1.5 * iqr
+tracks = tracks.filter((col("duration_ms") >= lower_bound) & (col("duration_ms") <= upper_bound))
+print("size "+ str(tracks.count()))
 sc.stop()
+# Generating statistics duration
+
+# STEP 
