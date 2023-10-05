0a051bed3b68ca879a896d6540e3fdc1ec5cd924
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2023-10-05 17:03:02 -0300

    feat: task 3

--- task-3.py
+++ task-3.py
@@ -1,28 +1,27 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.window import Window
-from pyspark.sql.functions import col, exp, desc, row_number,count
+from pyspark.sql.functions import col, exp, desc, row_number,count, max,format_number
 import matplotlib.pyplot as plt
 
 spark = SparkSession.builder.appName("HelloLines").getOrCreate()
 spark.sparkContext.setLogLevel("WARN")
 
+# Carrega os dados
 playlists = spark.read.json("hdfs://localhost:9000/datasets/spotify/playlist.json")
 tracks =   spark.read.json("hdfs://localhost:9000/datasets/spotify/tracks.json")
 
+# Calcula o número de músicas de cada playlist
+size = playlists.join(tracks, playlists["pid"] == tracks["pid"], ).groupBy(playlists["pid"]).agg(count(tracks.pid).alias("count_songs"))
 
-size = playlists.join(tracks, playlists["pid"] == tracks["pid"], ).groupBy(playlists["pid"]).count().alias("count_songs")
+# Calcula o número de músicas de cada artista em cada playlist
 artist_counts = playlists.join(tracks, playlists["pid"] == tracks["pid"], ) \
-                .groupBy(playlists["pid"],"artist_uri").agg(count("artist_uri") \
-                .alias("artist_count")).orderBy(desc("artist_count")).limit(5).show()
+                .groupBy(playlists["pid"],"artist_uri", "artist_name").agg(count("artist_uri") \
+                .alias("artist_count"))
 
-max_artist_count = artist_counts.groupBy("pid").agg(max(col("artist_count")).alias("max_artist_count"))
+# Seleciona o artista mais frequente de cada playlist
+max_artist_count = artist_counts.groupBy("pid", "artist_uri", "artist_name").agg(max(artist_counts.artist_count).alias("max_artist_count"))
 
+# Calcula a razão entre o número de músicas de um artista e o número total de músicas da playlist
 prevalence_df = size.join(max_artist_count, "pid").withColumn(
-    "artist_prevalence", col("max_artist_count") / col("artist_ocurrences")
-).select("pid", "artist_prevalence")
-
-# Show the resulting DataFrame
-prevalence_df.show()
-
-
-spark.close()
+    "artist_prevalence", format_number((col("max_artist_count") / size.count_songs)*100,2)
+).select("pid", "artist_prevalence", "artist_name").orderBy(desc("artist_prevalence")).show()
