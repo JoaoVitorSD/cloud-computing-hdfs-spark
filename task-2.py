8b6e2203f8f42571a9ad5da496b696a21782513a
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2023-10-05 15:51:50 -0300

    refac: improve organization

--- task-2.py
+++ task-2.py
@@ -7,22 +7,11 @@ spark = SparkSession.builder.appName("HelloLines").getOrCreate()
 spark.sparkContext.setLogLevel("WARN")
 
 playlists = spark.read.json("hdfs://localhost:9000/datasets/spotify/playlist.json")
-tracks =   spark.read.json("hdfs://localhost:9000/datasets/spotify/tracks.json")
+tracks = spark.read.json("hdfs://localhost:9000/datasets/spotify/tracks.json")
+
+tracks = tracks.join(playlists, tracks["pid"] == playlists["pid"], ).groupBy("artist_uri", "artist_name").count()
+tracks = tracks.orderBy(desc("count"))
+tracks.limit(5).show()
 
 
-size = playlists.join(tracks, playlists["pid"] == tracks["pid"], ).groupBy(playlists["pid"]).count().alias("count_songs")
-artist_counts = playlists.join(tracks, playlists["pid"] == tracks["pid"], ) \
-                .groupBy(playlists["pid"],"artist_uri").agg(count("artist_uri")
-.alias("artist_count")).orderBy(desc("artist_count")).limit(5).show()
-
-max_artist_count = artist_counts.groupBy("pid").agg(max(col("artist_count")).alias("max_artist_count"))
-
-prevalence_df = size.join(max_artist_count, "pid").withColumn(
-    "artist_prevalence", col("max_artist_count") / col("artist_ocurrences")
-).select("pid", "artist_prevalence")
-
-# Show the resulting DataFrame
-prevalence_df.show()
-
-
-spark.close()
+spark.stop()
