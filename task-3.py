5c24fc57892ec4c8023ba3d7f0aba6a2324dbad0
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2023-10-06 21:56:21 -0300

    feat: calculating CDF

--- task-3.py
+++ task-3.py
@@ -9,9 +9,9 @@ spark.sparkContext.setLogLevel("WARN")
 # Carrega os dados
 playlists = spark.read.json("hdfs://localhost:9000/datasets/spotify/playlist.json")
 tracks =   spark.read.json("hdfs://localhost:9000/datasets/spotify/tracks.json")
+playlist_count = playlists.count()
 
 # Calcula o número de músicas de cada playlist
-size = playlists.join(tracks, playlists["pid"] == tracks["pid"], ).groupBy(playlists["pid"]).agg(count(tracks.pid).alias("count_songs"))
 
 # Calcula o número de músicas de cada artista em cada playlist
 artist_counts = playlists.join(tracks, playlists["pid"] == tracks["pid"], ) \
@@ -22,6 +22,16 @@ artist_counts = playlists.join(tracks, playlists["pid"] == tracks["pid"], ) \
 max_artist_count = artist_counts.groupBy("pid", "artist_uri", "artist_name").agg(max(artist_counts.artist_count).alias("max_artist_count"))
 
 # Calcula a razão entre o número de músicas de um artista e o número total de músicas da playlist
-prevalence_df = size.join(max_artist_count, "pid").withColumn(
-    "artist_prevalence", format_number((col("max_artist_count") / size.count_songs)*100,2)
-).select("pid", "artist_prevalence", "artist_name").orderBy(desc("artist_prevalence")).show()
+prevalence_df = playlists.join(max_artist_count, "pid").withColumn(
+    "artist_prevalence", format_number((col("max_artist_count") / playlists.num_tracks)*100,2)
+).select(playlists["pid"], "artist_prevalence").orderBy("artist_prevalence")
+
+prevalence_collect = prevalence_df.collect()
+cdf_vector = []
+prevalence = []
+for playlist in prevalence_collect:
+    cdf = prevalence_df.filter(prevalence_df["artist_prevalence"] <= playlist.artist_prevalence).count()/playlist_count
+    cdf_vector.append(cdf)
+    prevalence.append(playlist.artist_prevalence)
+
+plt.plot(prevalence, cdf_vector, marker='o', linestyle='-')
